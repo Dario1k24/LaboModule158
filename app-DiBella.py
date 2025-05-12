d55de11eda0e98b68f5f0bca2c550ae1724c7565
Dario1k24 <dariodibella07@gmail.com>
2025-05-12 12:47:31 +0000

    Mon dernier test avec le final

--- app-DiBella.py
+++ app-DiBella.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "Dernier test du module 158"
+	return "Mon dernier test pour le labo"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
