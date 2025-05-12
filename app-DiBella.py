a4e40342ef980f415c46f07f8f16b5cbb17d1334
Dario1k24 <dariodibella07@gmail.com>
2025-05-12 14:58:30 +0000

    Message de test

--- app-DiBella.py
+++ app-DiBella.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "Mon dernier test pour le labo"
+	return "Module 158t"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
